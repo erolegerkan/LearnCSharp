80444aaf9eaee836e3db9f9518021e761607eaed
erolegerkan <erolege.education@hotmail.com>
2025-01-05 12:27:11 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program //Internal - access identifier
+    {
+        static void Main(string[] args)
+        {
+            #region Printing Commands
+
+            //Console.WriteLine("Hi again :)");// Print and then comes down to new line
+            //Console.Write("Hello World");// Print and then leave the cursor after context
+            //Console.Read(); // Hold the screen until user clicks the enter button from keyboard
+
+            //Console.WriteLine("***** Food Categories *****");
+            //Console.WriteLine();
+            //Console.WriteLine("1 - Soups");
+            //Console.WriteLine("2 - Main foods");
+            //Console.WriteLine("3 - Cold starters");
+            //Console.WriteLine("4 - Cold salads");
+            //Console.WriteLine("5 - Desserts");
+            //Console.WriteLine("6 - Drinks");
+            //Console.WriteLine();
+            //Console.WriteLine("***** Food Categories *****");
+
+            #endregion
+
+            #region String Variables
+
+            // string
+
+            //string name;
+            //name = "Ege";
+            //Console.WriteLine(name);
+
+            //string customerName; // camelCase
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Stanimir";
+            //customerSurname = "Stoilov";
+            //customerPhone = "+90 500 400 30 20";
+            //customerEmail = "stoilov@gmail.com";
+            //district = "Goztepe";
+            //city = "İzmir";
+
+            //Console.WriteLine("***** Rezervation Information *****");
+
+            //Console.WriteLine("Name : " + customerName);
+            //Console.WriteLine("Surname : " + customerSurname);
+            //Console.WriteLine("Phone: " + customerPhone);
+            //Console.WriteLine("Email: " + customerEmail);
+            //Console.WriteLine("District: " + district);
+            //Console.WriteLine("City: " + city);
+            #endregion
+
+            #region Int Variables
+
+            //int 
+
+            //int hamburgerPrice = 300;
+            //int cokePrice = 35;
+            //int cookiePrice = 30;
+            //int chickenWingsPrice = 300;
+            //int cakePrice = 100;
+
+            //Console.WriteLine("**** Food Menu ****");
+            //Console.WriteLine("Hamburger : " + hamburgerPrice);
+            //Console.WriteLine("Coke : " + cokePrice);
+            //Console.WriteLine("Cookie : " + cookiePrice);
+            //Console.WriteLine("Chicken Wings: " + chickenWingsPrice);
+            //Console.WriteLine("Cake : " + cakePrice);
+
+            #endregion
+        }
+    }
+}
+
+// Printing commands
